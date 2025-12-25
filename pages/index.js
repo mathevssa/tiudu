5db28061c6255466ce35ef8f8034f15baf57a0a0
Matheus Almeida <72870434+mathevssa@users.noreply.github.com>
2025-12-25 22:21:18 +0000

    Adiciona descritivo do TiuDu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Testando a Vercel</h1>;
+  return <h1>TiuDu</h1>;
+  <h2>Um task manager pessoal feito pra quem só quer anotar, lembrar e fazer. Com a rapidez de um bloco de notas, só que melhor.</h2>
+  <h3>Propósito</h3>
+  <p>TiuDu nasceu da frustração real de quem passou anos testando ferramentas de task management que exigem setups complexos, curvas de aprendizado longas ou excesso de recursos que atrapalham mais do que ajudam. O objetivo aqui é outro: criar o gerenciador de tarefas mais simples, prático e rápido possível, pensado para uso individual no dia a dia.
+Inspirado no caderno e na caneta como bloco de notas mais direto e eficiente, o TiuDu busca ser essa versão digital que respeita sua rotina e sua cabeça, sem distrações, sem frescura, sem overdesign.</p> 
+  <h3>O que o TiuDu quer ser:</h3>
+  <p>	•	Mais simples do que abrir o WhatsApp pra mandar uma anotação pra si mesmo
+	•	Mais prático que um bloco de papel, com o poder da tecnologia
+	•	Mais leve que os grandes task managers e com zero burocracia
+	•	Mais humano e focado em facilitar a vida de quem tá sozinho organizando suas ideias, não de times inteiros</p>
 }
 
 export default Home;
